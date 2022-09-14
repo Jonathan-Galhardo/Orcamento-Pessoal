07f1358881374c4958db73fa4b08a9e36db0fc45
Jonathan-Galhardo <galhardoj@gmail.com>
2022-09-14 15:41:20 -0300

    Otimização da janela modal

--- logica.js
+++ logica.js
@@ -8,34 +8,34 @@ class Despesa {
         this.valor = valor
     }
     validarDados() {
-        for(let i in this){
+        for (let i in this) {
             //console.log(i, this)
             if (this[i] == undefined || this[i] == '' || this[i] == null) {
                 return false
-            } 
-        } return true  
-        
+            }
+        } return true
+
     }
 }
 
 class Bd {
 
-    constructor (){
+    constructor() {
         let id = localStorage.getItem('id')
-        if (id === null){
+        if (id === null) {
             localStorage.setItem('id', 0)
         }
     }
 
     getProximoId() {
         let proximoId = localStorage.getItem('id')
-        return parseInt(proximoId) + 1 
+        return parseInt(proximoId) + 1
     }
-    
-    salvar(d){
+
+    salvar(d) {
         let id = this.getProximoId()
 
-        localStorage.setItem( id, JSON.stringify(d))
+        localStorage.setItem(id, JSON.stringify(d))
 
         localStorage.setItem('id', id)
     }
@@ -45,7 +45,7 @@ class Bd {
 let bd = new Bd()
 
 function cadastrarDespesa() {
-    
+
     let ano = document.getElementById('ano')
     let mes = document.getElementById('mes')
     let dia = document.getElementById('dia')
@@ -54,16 +54,27 @@ function cadastrarDespesa() {
     let valor = document.getElementById('valor')
 
     //console.log(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
-    
+
     let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
 
-    if(despesa.validarDados()){
+    if (despesa.validarDados()) {
         bd.salvar(despesa)
-        $('#sucessoGravacao').modal('show')
-        
+        $('#registraDespesa').modal('show')
+        document.getElementById('TituloModalCentralizado').innerHTML = 'Despesa cadastrada com sucesso!';
+        document.getElementById('modalbody').innerHTML = 'Todos os campos preenchidos!'
+        document.getElementById('button').innerHTML = 'OK'
+        document.getElementById('classe-titulo').className = 'modal-header text-success'
+        document.getElementById('button').className = 'btn btn-success'
     } else {
-        $('#erroGravacao').modal('show')
+        $('#registraDespesa').modal('show')
+        document.getElementById('TituloModalCentralizado').innerHTML = 'ERRO';
+        document.getElementById('modalbody').innerHTML = 'Certifique-se de preencher todos os campos antes de incluir uma despesa!'
+        document.getElementById('button').innerHTML = 'Voltar e Corrigir'
+        document.getElementById('classe-titulo').className = 'modal-header text-danger'
+        document.getElementById('button').className = 'btn btn-danger'
+
+
     }
-    
+
 }
 
