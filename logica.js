67fd45454d5d14467a78e386383dd87aad1d1eba
Jonathan-Galhardo <galhardoj@gmail.com>
2022-09-16 09:57:35 -0300

    Zerar campos de input após a gravação

--- logica.js
+++ logica.js
@@ -78,18 +78,28 @@ function cadastrarDespesa() {
     let descricao = document.getElementById('descricao')
     let valor = document.getElementById('valor')
 
+
     //console.log(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
 
     let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
 
     if (despesa.validarDados()) {
         bd.salvar(despesa)
+
         $('#registraDespesa').modal('show')
         document.getElementById('TituloModalCentralizado').innerHTML = 'Despesa cadastrada com sucesso!';
         document.getElementById('modalbody').innerHTML = 'Todos os campos preenchidos!'
         document.getElementById('button').innerHTML = 'OK'
         document.getElementById('classe-titulo').className = 'modal-header text-success'
         document.getElementById('button').className = 'btn btn-success'
+
+        ano.value = ''
+        mes.value = ''
+        dia.value = ''
+        tipo.value = ''
+        descricao.value = ''
+        valor.value = ''
+
     } else {
         $('#registraDespesa').modal('show')
         document.getElementById('TituloModalCentralizado').innerHTML = 'ERRO';
