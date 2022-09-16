2e588447790645a23907135e5e0aad45e1ca3907
Jonathan-Galhardo <galhardoj@gmail.com>
2022-09-16 10:30:39 -0300

    Função pesquisar Despesa

--- logica.js
+++ logica.js
@@ -151,6 +151,14 @@ function carregaListaDespesas() {
 }
 
 function pesquisaDespesa() {
-    console.log('blablabla')
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value
+    let tipo = document.getElementById('tipo').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+    console.log(despesa)
 
 }
